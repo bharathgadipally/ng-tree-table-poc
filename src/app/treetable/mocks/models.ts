979413e0ae0e0c2f91c7748d8784d061cbcfec15
Bharath Gadipally <bharath.gadipally@gmail.com>
2023-03-08 16:06:44 +0530

    initial commit

---
+++ src/app/treetable/mocks/models.ts
@@ -0,0 +1,45 @@
+export interface Folder {
+  name: string;
+  owner: string;
+  protected: boolean;
+  backup: boolean;
+}
+
+export interface Task {
+  name: string;
+  completed: boolean;
+  owner: string;
+}
+
+export interface TriggerEventsNode {
+  expandable: boolean;
+  startDateTime: Date;
+  endDateTime: Date;
+  recalculatedDate: Date;
+  authority: string;
+  description: string;
+  excluded: boolean;
+  locked: boolean;
+  delete: boolean;
+  jurisdictionId: string;
+  uniqueFormulaId: string;
+  groupId: string;
+  parentFormulaId: string;
+  eventId: number;
+  heirarchy: string;
+  cnsStatus: string;
+  hasChanges: boolean;
+  version: number;
+  level: number;
+  calculationSummary: string;
+  action: string;
+  eventSysId: number;
+  isManualDateOverride: boolean;
+  isManualTimeOverride: boolean;
+  categoryCode: string;
+  categoryName: string;
+  isPrimaryKeyEvent: boolean;
+  parent?: TriggerEventsNode;
+  selected?: boolean;
+  indeterminate?: boolean;
+}
